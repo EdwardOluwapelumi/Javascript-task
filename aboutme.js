68b1aa950ea3a0036e33effc97ec58b89c737e27
EdwardOluwapelumi <ogunwoledwardamilare@gmail.com>
2022-06-22 19:27:38 +0100

    About myself

---
+++ aboutme.js
@@ -0,0 +1,5 @@
+let myName = "Edward Ogunwole";
+let myHeight = "5'4";
+let myCountry = "Nigeria";
+
+alert("My name is " + myName + "\n" + "My height is " + myHeight + "\n" + "I am from " + myCountry);
